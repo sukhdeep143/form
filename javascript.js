506e42f53d0a2d25b870cb0d6fac33283ddff66e
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-08-11 00:18:58 +0530

    update

--- javascript.js
+++ javascript.js
@@ -1,41 +1,64 @@
 console.log("We are working with AJAX!!!");
 
-$("#getData").click(function () {
-    console.log("You clicked the GET button");
+$("#todoForm").submit(function (e) {
+    e.preventDefault()
+
+    let title = $("#title").val();
+    let userId = $("#userId").val();
+
+    console.log("Submitting Todo:", title, "for User:", userId);
 
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", 'test.txt', true);
-
-    xhr.onprogress = () => {
-        console.log("Progress...");
-    };
-
-    xhr.onload = function () {
-        if (this.status === 200) {
-            console.log(this.responseText);
-        } else {
-            console.error("Something went wrong!");
-        }
-    };
-
-    xhr.send();
-});
-
-$('#postData').click(() => {
-    console.log("You clicked the POST button");
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", 'test.txt', true);
+    xhr.open("POST", 'https://jsonplaceholder.typicode.com/todos', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.onload = function () {
-        if (this.status === 200) {
-            console.log(this.responseText);
+        if (this.status === 200) { 
+            console.log("Todo Created Successfully:");
+            console.log(JSON.parse(this.responseText));
         } else {
-            console.error("Something went wrong!");
+            console.error("Something went wrong!", this.status);
         }
     };
 
-    let param = JSON.stringify({ name: "sukhdeep" });
+    let param = JSON.stringify({
+        title: title,
+        userId: userId,
+        completed: false
+    });
+
     xhr.send(param);
 });
+
+
+
+$("#GetData").click(()=>{
+    console.log("Button is clicked");
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+
+    xhr.onload = function(){
+        if (this.status === 200) {
+              let todos = JSON.parse(this.responseText); 
+
+            let html = "<h3>Todos:</h3><ul>";
+            todos.forEach(todo => {
+                html += `<li>
+                            <strong>${todo.title}</strong> - 
+                            ${todo.title, todo.id}
+                         </li>`;
+            });
+            html += "</ul>";
+
+            $("#output").html(html);
+            
+        } else{
+            console.log("Something went wrong!!!")
+        }
+    }
+
+    xhr.send()
+    
+})
